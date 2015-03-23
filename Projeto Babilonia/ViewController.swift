1835f7f22fc2f7534e0415799b31efe12f7c9399
Matheus Mignoni <mmignoni@Matheuss-MacBook-Pro.local>
2015-03-23 10:00:39 -0300

    - Carregando alguma imagem da galeria de fotos e exibindo-a em um uiimageview

--- Projeto Babilonia/ViewController.swift
+++ Projeto Babilonia/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    /**
+    *   FUNCOES CRIADAS POR MIM
+    *
+    */
+    
+    //variavel do nosso uiimageview
+    @IBOutlet weak var lugarMostrarFoto: UIImageView!
+    
+    //funcao que é ativada ao apertar no botao "Galeria Fotos"
+    @IBAction func abrirGaleriaFotos(sender: AnyObject) {
+        var pegadorPhoto        = UIImagePickerController()
+        pegadorPhoto.delegate   = self
+        pegadorPhoto.sourceType = .PhotoLibrary
+        self.presentViewController(pegadorPhoto, animated: true, completion: nil)
+    }
+    
+    //Funcao para carregar a imagem para dentor do uiimageview
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingMediaWithInfo info: NSDictionary!) {
+        self.dismissViewControllerAnimated(true, completion: nil);
+        
+        let gotImage = info[UIImagePickerControllerOriginalImage] as UIImage
+        
+        self.lugarMostrarFoto.setValue(gotImage, forKey: "image")
+    }
 }
 
